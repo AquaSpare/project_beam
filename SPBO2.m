1b25e491ceeb04dd1a8a2ea235b6f57d7c968761
AquaSpare <39486044+AquaSpare@users.noreply.github.com>
2020-03-30 17:35:03 +0200

    SPB
    Andra ordningens SPB operator

---
+++ SPBO2.m
@@ -0,0 +1,24 @@
+
+function [H,M,e1,em,d1,dm] = SPBO2(N,h)
+
+H = diag(ones(N,1));
+H(1,1) = 1/2;
+H(end,end) = 1/2;
+H = h.*H;
+
+M = diag(2.*ones(N,1)) + diag(-1.*ones(N-1,1),1) + diag(-1.*ones(N-1,1),-1);
+M(1,1) = 1;
+M(end,end) = 1;
+M = h.*M;
+
+e1 = zeros(1,N);
+e1(1,1) = 1;
+em = zeros(1,N);
+em(1,end) = 1;
+
+d1 = zeros(1,N);
+d1(1,1:3) = [-3/2 2 1/2];
+d1 = d1.*(1/h);
+dm = zeros(1,N);
+dm(1,(end-2):end) = [1/2 -2 3/2];
+dm = dm.*(1/h);
