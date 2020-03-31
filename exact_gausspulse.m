79a468690588928034fa504fc82c58df4b246fdd
AquaSpare <39486044+AquaSpare@users.noreply.github.com>
2020-03-31 14:39:43 +0200

    Gausspuls
    grejer

--- exact_gausspulse.m
+++ exact_gausspulse.m
@@ -4,9 +4,15 @@ function u = exact_gausspulse(x,t,xp,r0,cl,cr,R,T)
 t_star = -xp/cl;
 
 if t<t_star
-    u = 0.5*exp(-(xp-(x+cl*t))^2/(r0^2)) + 0.5*exp(-(xp-(x-cl*t))^2/(r0^2)); 
+    u = 0.5*exp(-((xp-(x+cl*t)).^2)./(r0^2)) + 0.5*exp(-((xp-(x-cl*t)).^2)./(r0^2)); 
 else
-    u = 
+    xl = x(x<0);
+    xr = x(x<= 0);
+    
+    ul = 0.5*exp(-((x0-(xl+cl*t)).^2)./(r0^2)) + (R/2)*exp(-((xl+cl*(t-t_star)).^2)./(r0^2));
+    ur = (T/2)*exp(-((cl*(xr-cr)*(t-t_star)).^2)./(cr*r0)^2);
+    
+    u = [ul ur];
 end
 
         
