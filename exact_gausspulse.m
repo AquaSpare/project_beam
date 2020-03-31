7ea5dcc1ebd038e3ace6b8858e5fdcd85ba16702
AquaSpare <39486044+AquaSpare@users.noreply.github.com>
2020-03-31 17:14:21 +0200

    Lite buggar

--- exact_gausspulse.m
+++ exact_gausspulse.m
@@ -9,7 +9,7 @@ else
     xl = x(x<0);
     xr = x(x<= 0);
     
-    ul = 0.5*exp(-((x0-(xl+cl*t)).^2)./(r0^2)) + (R/2)*exp(-((xl+cl*(t-t_star)).^2)./(r0^2));
+    ul = 0.5*exp(-((xp-(xl+cl*t)).^2)./(r0^2)) + (R/2)*exp(-((xl+cl*(t-t_star)).^2)./(r0^2));
     ur = (T/2)*exp(-((cl*(xr-cr)*(t-t_star)).^2)./(cr*r0)^2);
     
     u = [ul ur];
