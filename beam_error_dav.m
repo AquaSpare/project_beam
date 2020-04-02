4f253904066f62162e9203957257dc08b508a231
DavidNiemela <56039238+DavidNiemela@users.noreply.github.com>
2020-04-02 14:27:10 +0200

    fuckat runt som fan med exakta lösningar
    asdfasdf

--- beam_error_dav.m
+++ beam_error_dav.m
@@ -1,4 +1,6 @@
-[good_solution,good_t,good_x] = beam_eq_projection_dav(101,0,0.5,1,0,0.01,1,1,2,2); 
+%[w,t,x,h,k] = beam_eq_projection_dav(N,x0,xl,xN,t0,T,b1,b2,order,BC)
+[good_solution1,good_t,good_x1] = beam_eq_projection_dav(51,0,0.5,1,0,0.1,1,1,4,2); 
+
 % [solution41,t41,x41] = beam_eq_projection_dav(41,-1,0,1,0,0.1,1,1,2,1);
 % [solution21,t21,x21] = beam_eq_projection_dav(21,-1,0,1,0,0.1,1,1,2,1);
 
