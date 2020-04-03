d71f75096275955e9e91a24d6278e1486cf81a00
NiklasWik <62885387+NiklasWik@users.noreply.github.com>
2020-04-03 17:45:53 +0200

    Summery (required)

--- beam_eq_projection.m
+++ beam_eq_projection.m
@@ -1,4 +1,23 @@
-function [w,t,x,h,k] = beam_eq_projection(N,x0,xl,xN,t0,T,b1,b2,order,BC)
+%%%% För exakt lösning krävs x0=0 xN=1, b1=b2=1 och bc=2 i.e
+%%%% beam_eq_projection(81,0,1/2,1,0.5,1,1,4,2,100,0.001);
+
+%%%% Några exempel:
+
+%%%% Clamped:
+%%%% beam_eq_projection(81,-1,0,1,0.1,1,100,4,1,200, 0.00001);
+
+%%%% Free:
+%%%% beam_eq_projection(81,-1,0,1,0.1,1,10,4,2,20, 0.0001);
+
+%%%% Sliding
+%%%% beam_eq_projection(81,-1,0,1,1,1,10,4,3,20, 0.0001);
+
+%%%% Hinged
+%%%% beam_eq_projection(81,-1,0,1,0.1,1,100,4,4,200, 0.00001);
+
+%%%% Stor skillnad i b kräver mindre k. När t.ex. b1=1, b2=100 krävs minst k = 0.00001*h.
+
+function [w,t,x,h,k] = beam_eq_projection(N, x0, xl, xN, T, b1, b2, order, BC, plotspeed, k_ratio)
 close all
 pause on
 %%%Domain%%%
@@ -9,13 +28,13 @@ x = [x1 x2];
 %%%%%%%%%%%%%%%%%%%%%%%%
 
 %%% Initial condition %%%
-if(b1 == b2) %%%%%%exact solution exists 
+if(b1 == b2 && BC == 2 && x0 == 0 && xN == 1) %%%%%%exact solution exists 
     u0 = @(x) cosh(1.50562*pi.*x) + cos(1.50562*pi.*x) - ((cos(1.50562*pi) -cosh(1.50562*pi))/(sin(1.50562*pi) - sinh(1.50562*pi)))*(sin(1.50562*pi.*x) +sinh(1.50562*pi.*x));
     u0_t = 0;
 else 
-    x0 = -1/4;
+    xr = -1/4;
     r0 = 1/30;
-    u0 = @(x) exp(-(x0-x).^2/r0^2);
+    u0 = @(x) exp(-(xr-x).^2/r0^2);
     u0_t = 0;
 end
 
@@ -48,19 +67,21 @@ A = (-P*[b1^2*D4 zeros(N); zeros(N) b2^2*D4]*P);
 w0 = [u0(x1) u0(x2)];
 w0_t = u0_t;
 
-[w,k,t] = timestepper(t0,T,h,A,w0,w0_t);
+[w,k,t] = timestepper(T,h,A,w0,w0_t, k_ratio);
 
 %%%Exact Solution%%%
 u_exact = @(x,t) real(exp(-1i*(22.3733)*t)*u0(x));
 figure(1);
-for i = 1:200:length(t)
-    plot(x1, w(1:N,i), '-b', x2, w(N+1:end,i), '-r');
-    axis([-1 1 -2 2])
-    if(b1 == b2)
+for i = 1:plotspeed:length(t)
+    if(b1 == b2 && BC == 2 && x0 == 0 && xN == 1)
+        plot(x1, w(1:N,i), '*b', x2, w(N+1:end,i), '*r');
         hold on;
         plot(x,u_exact(x,t(i)), 'g');
-        axis([0 1 -2 2])
+        axis([0 1 -2 2]);
         hold off;
+    else 
+        plot(x1, w(1:N,i), 'b', x2, w(N+1:end,i), 'r');
+        axis([x0 xN -1 1]);
     end
     pause(0.00000001);
 end
--- timestepper.m
+++ timestepper.m
@@ -1,13 +1,13 @@
-function [solution, k, t] = timestepper(t0, T, h, A, u0, u0_t)
+function [u, k, t] = timestepper(T, h, A, u0, u0_t, k_ratio)
 
 % k = 2*h/sqrt(max(abs(eig(A))));
-k = 0.00001*h;
+k = k_ratio*h;
 
-t = t0:k:T;
-solution = zeros(length(u0),length(t));
-solution(:,1) = u0;
-solution(:,2) = k*u0_t' + 0.5*k^2*A*u0' + u0';
+t = 0:k:T;
+u = zeros(length(u0),length(t));
+u(:,1) = u0;
+u(:,2) = k*u0_t' + 0.5*k^2*A*u0' + u0';
 for i=3:length(t)
-    solution(:,i) = k^2.*A*solution(:,i-1) + 2*solution(:,i-1) - solution(:,i-2);
+    u(:,i) = k^2.*A*u(:,i-1) + 2*u(:,i-1) - u(:,i-2);
 end
 
