1e9068b7eca019c0d128e14dbbfff521b0ad1a6b
DavidNiemela <56039238+DavidNiemela@users.noreply.github.com>
2020-04-14 15:12:49 +0200

    SAT PROJ JÄMFÖRELSE

--- SAT_enBalk.m
+++ SAT_enBalk.m
@@ -1,4 +1,4 @@
-function [u, t, x, h, k] = SAT_enBalk(N,x0,xN,T, b, order, BC, plotspeed, k_ratio)
+function [u, t, x, h, k] = SAT_enBalk(N,x0,xN,T, b, order, BC, plotspeed, k_ratio,plotornot)
 
 h = (xN-x0)/(N-1);
 x = x0:h:xN;
@@ -26,18 +26,21 @@ A = -D4 + SAT;
 [u,k,t] = timestepper(T,h,A,u0(x),u0_t,k_ratio);
 %%%Exact Solution%%%
 u_exact = @(x,t) real(exp(-1i*(22.3733)*t)*u0(x));
-figure(1);
-for i = 1:plotspeed:length(t)
-    if(BC == 2 && x0 == 0 && xN == 1)
-        plot(x, u(1:N,i), '*b');
-        hold on;
-        plot(x,u_exact(x,t(i)), 'g');
-        axis([0 1 -2 2]);
-        hold off;
-    else 
-        plot(x1, w(1:N,i), 'b', x2, w(N+1:end,i), 'r');
-        axis([x0 xN -1 1]);
+if plotornot == 1
+    figure(1);
+    for i = 1:plotspeed:length(t)
+        if(BC == 2 && x0 == 0 && xN == 1)
+          plot(x, u(1:N,i), '*b');
+           hold on;
+           plot(x,u_exact(x,t(i)), 'g');
+           axis([0 1 -2 2]);
+           hold off;
+        else 
+         plot(x1, w(1:N,i), 'b', x2, w(N+1:end,i), 'r');
+         axis([x0 xN -1 1]);
+        end
+     pause(0.00000001);
     end
-    pause(0.00000001);
 end
+
 
