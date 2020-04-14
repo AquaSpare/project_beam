94856667d3e476c8744ffa1770560d5cb2752d3f
AquaSpare <39486044+AquaSpare@users.noreply.github.com>
2020-04-14 11:29:20 +0200

    shit

--- homo_beam_error.m
+++ homo_beam_error.m
@@ -7,20 +7,37 @@ u_exact = @(x,t) real(exp(-1i*(22.3733)*t)*u0(x));
 
 %konvergens för SBP4 
 
-iter = 6;
+iter = 5;
 N = 10;
 error = zeros(1,iter);
 steps = zeros(1,iter);
 
-figure(1)
+
 for i = 1:iter
-    [sol,t,x,h,k] = beam_homogenous((N*(i+1))+1,0,1,0.2,1,4,4,500,0.00001,0);
+    [sol,t,x,h,k] = beam_homogenous((N*i)+1,0,1,0.2,1,4,4,500,0.00001,0);
     exact = u_exact(x,t(end));
     steps(i) = h;
     error(i) = sqrt(1/(N*i))*norm(exact-sol(:,end),2);
 end
 
-figure(2)
+figure(1)
 loglog(steps,error,'r*')
 xlabel('log(h)')
+ylabel('L2 norm of error')
+
+%undersöka felet över tid
+
+[sol,t,x,h,k] = beam_homogenous(41,0,1,10,1,6,4,500,0.0001,0);
+exact = zeros(size(sol));
+errortime = zeros(1,length(t));
+
+for i = 1:length(t)
+    exact(:,i) = u_exact(x,t(i));
+    errortime(1,i) = sqrt(1/40)*norm(exact(:,i) - sol(:,i),2);        
+end
+
+figure(2)
+
+plot(t,errortime)
+xlabel('time')
 ylabel('L2 norm of error')
