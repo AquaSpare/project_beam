bd1a26f095d20bf405869fe938425e97828b51c7
DavidNiemela <56039238+DavidNiemela@users.noreply.github.com>
2020-04-24 13:31:56 +0200

    Fixat analytisk lösning homogen

--- beam_homogenous.m
+++ beam_homogenous.m
@@ -15,18 +15,8 @@ x = x0:h:xl;
 
 %initial condition
 if b == 1 && x0 == 0 && xl == 1
-    if BC == 2
-        Beta = 4.73004074486270402602404810;
-        u0 = @(x) cosh(Beta.*x) + cos(Beta.*x) - ((cos(Beta) -cosh(Beta))/(sin(Beta) - sinh(Beta)))*(sin(Beta.*x) +sinh(Beta.*x));
-        w0_t = 0;
-        w0 = u0(x);
-        special_case = 1;
-    elseif BC == 1
-        Beta = 4.73004074486270402602404810;
-        u0 = @(x) cosh(Beta.*x) - cos(Beta.*x) - (sinh(Beta)+sin(Beta))/(cosh(Beta)-cos(Beta))*(sinh(Beta.*x)-sin(Beta*x));
-        w0_t = 0;
-        w0 = u0(x);
-        special_case = 1;
+    [w_exact,w0,w0_t] = homo_beam_ana(x,BC);
+    special_case = 1;
     else
     xp = 0.5;
     r0 = 0.05;
@@ -51,14 +41,14 @@ if BC == 1
     %------Clamped-------
     L = [e1;eN;d1_1;dN_1];
 elseif BC == 2
-    %------Hinged--------
-    L = [e1;eN;d1_2;dN_2];
+    %-------Free---------
+    L = [d1_2;dN_2;d1_3;dN_3];
 elseif BC == 3
     %------Sliding-------
     L = [d1_1;dN_1;d1_3;dN_3];
 elseif BC == 4
-    %-------Free---------
-    L = [d1_2;dN_2;d1_3;dN_3];
+    %------Hinged--------
+    L = [e1;eN;d1_2;dN_2];
 end
     
     
@@ -70,8 +60,6 @@ if timestepperversion == 1
 elseif timestepperversion == 2
     [w,k,t] = timestepperv2(T,h,A,w0,w0_t,k_ratio);
 end
-%exact soliution for special case
-u_exact = @(x,t) real(exp(-1i*(Beta^2)*t)*u0(x));
 
 if plotornot == 1 && special_case == 0
     for i = 1:plotspeed:length(t)
@@ -83,8 +71,8 @@ elseif plotornot == 1 && special_case == 1
     for i = 1:plotspeed:length(t)
         plot(x,w(:,i),'*')
         hold on
-        plot(x,u_exact(x,t(i)))
-        axis([0 1 -5 5])
+        plot(x,w_exact(x,t(i)))
+        axis([0 1 -3 3])
         pause(0.00000001)   
         hold off
     end
