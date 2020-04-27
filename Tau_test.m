c9adc2a6ea16dbf17c665c71ef6cc78ef72a87dc
NiklasWik <62885387+NiklasWik@users.noreply.github.com>
2020-04-27 13:08:13 +0200

    ehm

--- Tau_test.m
+++ Tau_test.m
@@ -1,4 +1,6 @@
-N = 81; x0 = -1; xl =0; xN = 1; a1 = 1; a2 = 4; order = 4; BC = 2; k_ratio = 0.001;
+close all;
+clear all;
+N = 41; x0 = -1; xl =0; xN = 1; a1 = 1; a2 = 100; order = 4; BC = 2; k_ratio = 0.00095;
 
 h = (xN-xl)/(N-1);
 x1 = x0:h:xl;
@@ -18,11 +20,11 @@ elseif(order == 6)
 end
 
 
-tau5 = linspace(-100, 100, 201);
-tau6 = linspace(-100, 100, 201);
+tau5 = linspace(0.99,100,100);
+tau6 = 1;
 tau3 = 1 + (tau5-1)*a2/a1;
 tau4 = 1 + (tau6-1)*a2/a1;
-maxeig = zeros(length(tau5),length(tau6));
+maxeig = zeros(length(tau5),1);
 
 L = [eN -e1; dN_1 -d1_1];
 H = [H zeros(N); zeros(N) H];
@@ -30,14 +32,14 @@ HI2 = inv(H);
 P = [eye(N) zeros(N); zeros(N) eye(N)] - HI2*L'*inv(L*HI2*L')*L;
 
 for i=1:length(tau5)
-   for j=1:length(tau6)
-        l_u = -a1*D4 - a1*HI*(-d1_1'*d1_2 + e1'*d1_3 - tau3(i).*eN'*dN_3 + tau4(j).*dN_1'*dN_2);
-        r_l = -a2*D4 - a2*HI*(-tau6(j).*d1_1'*d1_2 + tau5(i).*e1'*d1_3 - eN'*dN_3 + dN_1'*dN_2);
+   %for j=1:length(tau6)
+        l_u = -a1*D4 - a1*HI*(-d1_1'*d1_2 + e1'*d1_3 - tau3(i)*eN'*dN_3 + tau4*dN_1'*dN_2);
+        r_l = -a2*D4 - a2*HI*(-tau6*d1_1'*d1_2 + tau5(i)*e1'*d1_3 - eN'*dN_3 + dN_1'*dN_2);
         A = P*[l_u zeros(N); zeros(N) r_l]*P;
-        maxeig(i,j) = max(abs(eig(k^2.*A+2*eye(size(A)))));
-   end
+        maxeig(i) = max(abs(eig(k^2*A+2*eye(size(A)))));
+   %end
 end
-mesh(tau5,tau6,maxeig);
+plot(tau5,maxeig);
 xlabel('Tau5');
 ylabel('Tau6');
 zlabel('Maxeig');
