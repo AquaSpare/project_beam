856d5b0410c6a1b94f80773f33e67b8dd0f5bf01
DavidNiemela <56039238+DavidNiemela@users.noreply.github.com>
2020-04-27 16:49:25 +0200

    fixat fel grejer

--- SATProj_Proj_error.m
+++ SATProj_Proj_error.m
@@ -10,23 +10,33 @@ stepsSAT = zeros(1,iter);
 stepsProjSAT = zeros(1,iter);
 errorProjSAT = zeros(1,iter);
 
-[u,v] = beam_ana();
+BC = 2;
+a1 = 1;
+a2 = 4;
+T = 0.14;
+order = 4;
+
+x0 = -1;
+xl = 0;
+xN = 1;
+
+[u,v] = beam_ana(BC,a1,a2);
 
 for i = 1:iter
-    [solProj,tProj,xProj,hProj,kProj] = proj_proj_proj((N*i)+1,-1,0,1,0.14,1,4,4,2,100,0.0001,1,0,2);
-    exact = [u(xProj(1:(N*i)+1),tProj(end)) v(xProj((N*i)+2:end),tProj(end))];
+    [solProj,tProj,xProj,hProj,kProj] = proj_proj_proj((N*i)+1, x0, xl, xN, T, a1, a2, order, BC, 10, 0.0001, 1, 0, 2);
+    exact = [u(x0:hProj:xl,tProj(end)) v(xl:hProj:xN,tProj(end))];
     stepsProj(i) = hProj;
-    errorProj(i) = sqrt(1/(N*i))*norm(exact'-solProj(:,end),2);
+    errorProj(i) = sqrt(hProj)*norm(exact'-solProj(:,end),2);
                                         
-    [solSAT, tSAT, xSAT, hSAT, kSAT] = SAT_projSAT_SAT((N*i)+1,-1,0,1,0.14, 1, 4, 4, 2, 100,0.0001,1,0,2);
-    exact = [u(xSAT(1:(N*i)+1),tSAT(end)) v(xSAT((N*i)+2:end),tSAT(end))];
+    [solSAT, tSAT, xSAT, hSAT, kSAT] = SAT_projSAT_SAT((N*i)+1,x0,xl,xN,T, a1, a2, order, BC, 10,0.0001,1,0,2);
+    exact = [u(x0:hSAT:xl,tSAT(end)) v(xl:hSAT:xN,tSAT(end))];
     stepsSAT(i) = hSAT;
-    errorSAT(i) = sqrt(1/(N*i))*norm(exact'-solSAT(:,end),2);
+    errorSAT(i) = sqrt(hSAT)*norm(exact'-solSAT(:,end),2);
     
-    [solProjSAT, tProjSAT, xProjSAT, hProjSAT, kProjSAT] = proj_projSAT_proj((N*i)+1,-1,0,1,0.14, 1, 4, 4, 2, 100,0.0001,1,0,2);
-    exact = [u(xProjSAT(1:(N*i)+1),tProjSAT(end)) v(xProjSAT((N*i)+2:end),tProjSAT(end))];
+    [solProjSAT, tProjSAT, xProjSAT, hProjSAT, kProjSAT] = proj_projSAT_proj((N*i)+1,x0,xl,xN,T, a1, a2, order, BC, 100,0.0001,1,0,2);
+    exact = [u(x0:hProjSAT:xl,tProjSAT(end)) v(xl:hProjSAT:xN,tProjSAT(end))];
     stepsProjSAT(i) = hProjSAT;
-    errorProjSAT(i) = sqrt(1/(N*i))*norm(exact'-solProjSAT(:,end),2);
+    errorProjSAT(i) = sqrt(hProjSAT)*norm(exact'-solProjSAT(:,end),2);
 end
 figure(1)
 loglog(stepsProj,errorProj,'r*')
