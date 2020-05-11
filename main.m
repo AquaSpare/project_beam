fb2d842bac0f7dd88126e7cd637b0a90be13b164
NiklasWik <62885387+NiklasWik@users.noreply.github.com>
2020-05-11 13:17:37 +0200

    hh

--- main.m
+++ main.m
@@ -4,7 +4,8 @@ ff = @(x) func(x,a1,a2,BC);
 x0 = ones(10,1);
 x0(1) = ini;
 x0(2) = ini;
-options = optimoptions('fsolve','FunctionTolerance',1e-15);
+x0(3:10) = 10*rand(8,1);
+options = optimoptions('fsolve','FunctionTolerance',1e-12);
 x = fsolve(ff,x0,options);
 
 ff(x)
